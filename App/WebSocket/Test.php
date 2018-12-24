628128d9254f582cee765129990fff8db8b75ba9
XSOOY-20170812I\Administrator <lianghanrong@gmail.com>
2018-12-24 17:19:54 +0800

    1

--- App/WebSocket/Test.php
+++ App/WebSocket/Test.php
@@ -152,7 +152,7 @@ class Test extends Controller
 
         $tableCount = TableManager::getInstance()->get("room_count");
 
-        if($tableCount['count']>=10){
+        if($tableCount->get("0")["count"]>=10){
             $this->response()->setMessage("房间达到数量上限");
         }
         else {
